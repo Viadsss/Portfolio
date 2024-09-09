1675dab19420416d95856a123859f2c50cf2be94
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-09 22:05:30 +0800

    Add navbar button & mobile navbar

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,7 +1,8 @@
 import useTheme from "../hooks/useTheme";
-import { IconSun, IconMoonStars, IconFileText } from "@tabler/icons-react";
+import { IconSun, IconMoonStars } from "@tabler/icons-react";
 import VlogoLight from "../assets/V - light.svg";
 import VLogoDark from "../assets/V - dark.svg";
+import { useState } from "react";
 
 const links = [
   { name: "Home", href: "#" },
@@ -13,8 +14,10 @@ const links = [
 
 const Navbar = () => {
   const { theme, toggleTheme } = useTheme();
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
   return (
-    <header className="flex h-16 items-center justify-between border-b border-blue-500/30 bg-zinc-100 px-8 backdrop-blur-0 transition dark:border-violet-600/30 dark:bg-zinc-900">
+    <header className="relative flex h-16 items-center justify-between border-b border-blue-500/30 bg-zinc-100 px-8 backdrop-blur-0 transition dark:border-violet-600/30 dark:bg-zinc-900">
       <a className="f flex items-center gap-x-2" href="/">
         <img
           src={VLogoDark}
@@ -28,7 +31,7 @@ const Navbar = () => {
         />
         <div className="text-2xl font-bold">Viads</div>
       </a>
-      <nav>
+      <nav className="hidden md:block">
         <ul className="flex gap-x-8">
           {links.map((link) => (
             <li key={link.name}>
@@ -42,23 +45,48 @@ const Navbar = () => {
           ))}
         </ul>
       </nav>
-      <div className="flex items-center gap-x-3">
+      <div className="mr-2 flex items-center gap-x-8">
         <button
           onClick={toggleTheme}
           className="rounded-full p-1 transition-all hover:scale-110 hover:bg-blue-600 hover:text-white dark:hover:bg-violet-600"
         >
           {theme === "light" ? <IconMoonStars /> : <IconSun />}
         </button>
-        <a
-          href="/resume.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ml-4 flex items-center gap-x-2 rounded bg-blue-600 px-4 py-2 text-white transition dark:bg-violet-600 dark:hover:bg-violet-700"
+        <button
+          className="relative h-8 w-8 md:hidden"
+          onClick={() => setIsNavbarOpen(!isNavbarOpen)}
         >
-          <IconFileText />
-          <span>Resume</span>
-        </a>
+          <div className="absolute left-1/2 top-1/2 block w-5 -translate-x-1/2 -translate-y-1/2 transform">
+            {/* Top line */}
+            <span
+              className={`absolute block h-0.5 w-5 transform bg-current transition ${isNavbarOpen ? "rotate-45" : "-translate-y-1.5"}`}
+            ></span>
+            {/* Middle line */}
+            <span
+              className={`d absolute block h-0.5 w-5 transform bg-current transition ${isNavbarOpen ? "opacity-0" : ""}`}
+            ></span>
+            {/* Bottom line */}
+            <span
+              className={`absolute block h-0.5 w-5 transform bg-current transition ${isNavbarOpen ? "-rotate-45" : "translate-y-1.5"}`}
+            ></span>
+          </div>
+        </button>
       </div>
+
+      <nav
+        className={`absolute left-0 top-16 w-full border-b border-blue-500/30 bg-zinc-100 transition md:hidden dark:border-violet-600/30 dark:bg-zinc-900 ${isNavbarOpen ? "opacity-100" : "opacity-0"}`}
+      >
+        <ul className="text-center">
+          {links.map((link) => (
+            <li
+              key={link.name}
+              className="py-4 transition duration-200 hover:bg-blue-600 hover:text-white dark:hover:bg-violet-600"
+            >
+              <a href={link.href}>{link.name}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
