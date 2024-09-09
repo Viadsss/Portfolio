82942c36d88166f6350ca0e69481dcc4881e7c62
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-09 19:41:46 +0800

    Add opacity for large screen

--- src/components/CursorTrack.tsx
+++ src/components/CursorTrack.tsx
@@ -4,13 +4,11 @@ import amongusGif from "../assets/among-us.gif";
 const CursorTrack = () => {
   const cursorRef = useCursorImage(16, 16, 10000);
 
-  console.log("Rendering CursorTrack");
-
   return (
     <div className="relative">
       <div
         ref={cursorRef}
-        className="pointer-events-none absolute h-16 w-16 -translate-x-1/2 -translate-y-1/2 transform"
+        className="pointer-events-none absolute h-16 w-16 -translate-x-1/2 -translate-y-1/2 transform opacity-30 md:opacity-100"
       >
         <img src={amongusGif} alt="Among Us gif" className="h-full w-full" />
       </div>
