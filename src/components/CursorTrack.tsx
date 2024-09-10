5748d5359d6f4a6b35bfdf5ea2ae738f883c6901
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-10 12:32:52 +0800

    Import the hook and set the display to absolute

--- src/components/CursorTrack.tsx
+++ src/components/CursorTrack.tsx
@@ -1,11 +1,11 @@
-import useCursorImage from "../hooks/useCusrorImage";
+import useCursorImage from "../hooks/useCursorImage";
 import amongusGif from "../assets/among-us.gif";
 
 const CursorTrack = () => {
   const cursorRef = useCursorImage(16, 16, 10000);
 
   return (
-    <div className="relative">
+    <div className="absolute">
       <div
         ref={cursorRef}
         className="pointer-events-none absolute h-16 w-16 -translate-x-1/2 -translate-y-1/2 transform opacity-30 md:opacity-100"
