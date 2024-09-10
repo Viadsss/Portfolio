f5a65f3f6401a1aa4f3a05886b1ba239c1922b01
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-10 21:50:41 +0800

    Add custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ export default {
         sora: ["Sora", "sans-serif"],
         martianMono: ["Martian Mono", "system-ui"],
       },
+      keyframes: {
+        morph: {
+          "0%, 100%": {
+            borderRadius: "60% 40% 30% 70% / 60% 30% 70% 40%",
+          },
+          "50%": {
+            borderRadius: "30% 60% 70% 40% / 50% 60% 30% 60%",
+          },
+        },
+      },
+      animation: {
+        morph: "morph 5s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
