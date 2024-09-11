1c34a614c12c309cc298d463e7bd3b1842a004bb
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-11 18:29:19 +0800

    Add color to icons when hovered

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import CursorTrack from "./components/CursorTrack";
 import About from "./layout/About";
+import Contact from "./layout/Contact";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
 import Home from "./layout/Home";
-import Navbar from "./layout/Navbar";
 import Projects from "./layout/Projects";
+import Skills from "./layout/Skills";
 
 export default function App() {
   console.log("Rendering App");
@@ -11,10 +14,13 @@ export default function App() {
     <>
       <div className="grid-gradient-white dark:grid-gradient-dark relative h-full min-h-screen bg-zinc-100 font-sora text-black transition dark:bg-zinc-900 dark:text-white">
         <CursorTrack />
-        <Navbar />
+        <Header />
         <Home />
-        <About />
         <Projects />
+        <About />
+        <Skills />
+        <Contact />
+        <Footer />
       </div>
     </>
   );
--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -12,7 +12,7 @@ const Home = () => {
       id="home"
     >
       <div className="flex flex-col items-center justify-center gap-4 px-6 py-8 text-center backdrop-blur-0 lg:my-12 lg:flex-row-reverse lg:text-left">
-        <div className="animate-morph my-8 aspect-square w-full max-w-80 basis-1/2 border-4 border-blue-800 bg-[url('/Me.jpg')] bg-cover bg-center dark:border-violet-800"></div>
+        <div className="my-8 aspect-square w-full max-w-80 basis-1/2 animate-morph border-4 border-blue-800 bg-[url('/Me.jpg')] bg-cover bg-center dark:border-violet-800"></div>
         <div className="basis-1/2 space-y-8">
           <h1 className="bg-gradient-to-r from-blue-800 to-blue-400 bg-clip-text text-5xl font-bold tracking-widest text-transparent lg:text-6xl dark:from-violet-600 dark:to-white">
             Full Stack Developer
@@ -27,7 +27,7 @@ const Home = () => {
           </p>
           <div className="flex items-center justify-center gap-x-6 text-xl lg:justify-start">
             <a
-              className="transition hover:-translate-y-1 hover:text-violet-400"
+              className="transition hover:-translate-y-1 hover:text-blue-600 dark:hover:text-violet-400"
               href=""
               target="_blank"
               rel="noopener noreferrer"
@@ -35,7 +35,7 @@ const Home = () => {
               <IconBrandGithub className="h-10 w-10" />
             </a>
             <a
-              className="transition hover:-translate-y-1 hover:text-violet-400"
+              className="transition hover:-translate-y-1 hover:text-blue-600 dark:hover:text-violet-400"
               href=""
               target="_blank"
               rel="noopener noreferrer"
@@ -43,7 +43,7 @@ const Home = () => {
               <IconBrandLinkedin className="h-10 w-10" />
             </a>
             <a
-              className="transition hover:-translate-y-1 hover:text-violet-400"
+              className="transition hover:-translate-y-1 hover:text-blue-600 dark:hover:text-violet-400"
               href="mailto:johnpaulviado20@gmail.com"
               target="_blank"
               rel="noopener noreferrer"
@@ -51,7 +51,7 @@ const Home = () => {
               <IconMail className="h-10 w-10" />
             </a>
             <a
-              className="transition hover:-translate-y-1 hover:text-violet-400"
+              className="transition hover:-translate-y-1 hover:text-blue-600 dark:hover:text-violet-400"
               href="/resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
