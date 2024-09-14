753e26f28bad5e1e39e6de39238c522f9a3f3a38
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-14 16:35:22 +0800

    Modify SocialIcon to have rounded prop

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -2,12 +2,18 @@ type SocialIconProps = {
   href: string;
   label: string;
   icon: React.ReactNode;
+  rounded?: boolean;
 };
 
-const SocialIcon: React.FC<SocialIconProps> = ({ href, label, icon }) => {
+const SocialIcon: React.FC<SocialIconProps> = ({
+  href,
+  label,
+  icon,
+  rounded,
+}) => {
   return (
     <a
-      className="transition hover:-translate-y-1 hover:text-blue-600 dark:hover:text-violet-400"
+      className={`transition ${rounded ? "rounded-full p-1 hover:bg-blue-600 hover:text-white hover:dark:bg-violet-800" : "hover:-translate-y-1 hover:text-blue-600 dark:hover:text-violet-400"}`}
       href={href}
       target="_blank"
       rel="noopener noreferrer"
