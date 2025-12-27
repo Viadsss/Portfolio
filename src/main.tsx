75b2e845430a9142b24b084a55e371a2963fb379
John Paul <johnpaulviado20@gmail.com>
2025-12-28 01:11:04 +0800

    feat: add Vercel Analytics for enhanced performance tracking

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import "./index.css";
 import "react-photo-view/dist/react-photo-view.css";
 import router from "./router";
 import { ThemeProvider } from "./components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
       <RouterProvider router={router} />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>
 );
