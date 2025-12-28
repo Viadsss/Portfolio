07377a0c900587f8fbc81b877f004e0b73107ec7
John Paul <johnpaulviado20@gmail.com>
2025-12-28 11:23:10 +0800

    feat: add scroll-to-top functionality with animated button in AppLayout component

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -1,16 +1,24 @@
 import { ScrollRestoration } from "react-router";
 import { Footer } from "./footer";
 import { Header } from "./header";
+import { ChevronUp } from "lucide-react";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export function AppLayout({ children }: Props) {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
       <Header />
       <main className="mx-auto flex max-w-3xl flex-col px-8 pt-4 pb-12">{children}</main>
+      <div onClick={scrollToTop} className="mx-auto w-full p-4 opacity-30 transition hover:opacity-100">
+        <ChevronUp className="mx-auto animate-bounce" />
+      </div>
       <Footer />
       <ScrollRestoration />
     </>
